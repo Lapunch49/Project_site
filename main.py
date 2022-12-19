2d65cd89af8e4c064bee5a4289594c5b23ef9bb6
Lapunch49 <bulatovach@gmail.com>
2022-12-19 08:48:18 +0300

    add function to work with db

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
-from processor.dataprocessorfactory import *
+from processor.dataprocessorfactory import *    # подключаем фабрику обработчиков данных
+from repository.connectorfactory import *       # подключаем фабрику коннекторов к БД
+from repository.sql_api import *                # подключаем API для работы с БД
+import time
+
+DATASOURCE = "recipes.csv"
+DB_URL = 'sqlite:///test.db'
 
 """
     Пример простейшей функции, которая запускает обработчик данных и выводит результат обработки (возвращает None).
@@ -24,9 +30,21 @@ def init_processor(source: str) -> DataProcessor:
 def run_processor(proc: DataProcessor) -> None:
     proc.run()
     proc.print_result()
+    return proc.result
 
 
 if __name__ == '__main__':
-    proc = init_processor("recipes.csv")
+    result = None   # результат обработки данных
+    # Запуск обработчика данных
+    proc = init_processor(DATASOURCE)
     if proc is not None:
-        run_processor(proc)
+        result = run_processor(proc)
+
+# Работа с БД
+if result is not None:
+    db_connector = SQLStoreConnectorFactory().get_connector(DB_URL)   # получаем объект соединения
+    insert_into_source_files(db_connector, DATASOURCE)                # сохраняем в БД информацию о файле с набором данных
+    print(select_all_from_source_files(db_connector))                 # вывод списка всеъ обработанных файлов
+    insert_rows_into_processed_data(db_connector, result.iloc[:5], DATASOURCE)     # записываем в БД 5 первых строк результата
+    # Завершаем работу с БД
+    db_connector.close()
